77e1c7fa5d096aeeb798c2b37848c0cad598e48e
pichgii624@gmail.com <pichgii624@gmail.com>
2023-02-03 13:54:47 +0700

    change charector

--- POS_System/Program.cs
+++ POS_System/Program.cs
@@ -17,6 +17,7 @@ namespace POS_System
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //helooo
         }
     }
 }
